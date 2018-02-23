7cd167ce66cf9050b32a76edcec8af503ca3363a
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2018-02-23 17:34:08 -0500

    login and signup works

--- cardSite/settings.py
+++ cardSite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL = '/cards'
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
--- cardSite/urls.py
+++ cardSite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^cards/', include('cards.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login),
+    url(r'^logout/$', auth_views.logout)
 ]
